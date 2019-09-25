09d3b032e3b67717919194c16e04d1d182cf9dff
herrenchek <giselle.herr@icloud.com>
2019-09-25 01:02:15 -0400

    Seeded database

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Storefront
-An Amazon-like storefront created with Node.js and MySQL.
+An Amazon-like storefront created with Node.js and MySQL. The app takes in orders from customers and depletes stock from the store's inventory.
 
 View repository [here](https://github.com/herrenchek/Storefront).
 
--- schema.sql
+++ schema.sql
@@ -5,30 +5,40 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-item_id INT,
+item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(45) NOT NULL,
 department_name VARCHAR(45) NOT NULL,
-price INT,
-stock_quantity INT
+price DECIMAL(10,2) NOT NULL,
+stock_quantity INT(10) NOT NULL,
+PRIMARY KEY(item_id)
 );
 
--- CREATE TABLE products (
---   id INT NOT NULL AUTO_INCREMENT,
---   flavor VARCHAR(45) NULL,
---   price DECIMAL(10,2) NULL,
---   quantity INT NULL,
---   PRIMARY KEY (id)
--- );
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("House of Sand and Fog", "Books", 14.95, 9);
 
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("vanilla", 2.50, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The Martian", "Books", 13.95, 5);
 
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("chocolate", 3.10, 120);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sekiro: Shadows Die Twice", "Video Games", 59.99, 100);
 
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("strawberry", 3.25, 75);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Vitamix Blender", "Home & Kitchen", 499.95, 25);
 
--- ### Alternative way to insert more than one row
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone 11", "Electronics", 699.00, 999);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Death Note", "Anime", 40.00, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fancy Feast", "Pet Food", 14.72, 38);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Neon Genesis Evangelion", "Anime", 199.99, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("In Rainbows", "CDs & Vinyl", 22.45, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Demon's Souls", "Video Games", 25.00, 80);
