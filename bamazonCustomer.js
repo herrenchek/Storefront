79c9174f7698c8266232bcab415198c3f94d1668
herrenchek <giselle.herr@icloud.com>
2019-09-25 02:41:51 -0400

    Dotenv

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
+  user: process.env.USER,
   password: process.env.PASSWORD,
   database: "bamazon"
 });
